0823d325877dc96b73f3bf53cfdfb128ce991185
狼牙 <langya466@gmail.com>
2024-07-31 17:49:02 +0800

    Update ComboValue.java

--- src/org/union4dev/base/value/impl/ComboValue.java
+++ src/org/union4dev/base/value/impl/ComboValue.java
@@ -35,6 +35,19 @@ public class ComboValue extends AbstractValue<String> {
         for (String val : strings) if (val.equalsIgnoreCase(value)) super.setValue(val);
     }
 
+    public String getNextValue() {
+        ArrayList<String> valuesList = getAsArray();
+        String currentValue = getValue();
+        int index = valuesList.indexOf(currentValue);
+        if (index == -1 || index == valuesList.size() - 1) {
+            // 当前值不存在或者是最后一个值，则返回第一个值作为下一个值
+            return valuesList.getFirst();
+        } else {
+            // 返回下一个值
+            return valuesList.get(index + 1);
+        }
+    }
+
     public String[] getStrings() {
         return strings;
     }
